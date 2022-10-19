4d16a2890e8ce6289dbd6392ea6285af0e0b8906
thebill1000 <williamsbilljnr100@gmail.com>
2022-10-19 16:05:18 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,7 +2,6 @@
 
 /**
  * times_table -  a function that prints the 9 times table, starting with 0
- *
  * rone = row, cone = column, d = digits of current result
  * Return: times table
  * add extra space past single digit
@@ -24,16 +23,17 @@ void times_table(void)
 				_putchar((d / 10) + '0');
 			}
 			else
-				_putchar(');
-		}
-		_putchar((d % 10) + '0');
+			{
+				_putchar(' ');
+			}
+			_putchar((d % 10) + '0');
 
-		if (cone < 9)
-		{
-			_putchar(',');
-			_putchar(' ');
+			if (cone < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
-}
 }
