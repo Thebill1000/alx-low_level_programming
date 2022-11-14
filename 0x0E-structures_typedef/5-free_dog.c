df85b58a8f7a8853d2e738b440f910f5b19182ed
thebill1000 <williamsbilljnr100@gmail.com>
2022-11-14 06:56:25 -0800

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_dog - Entry Point
+ * @d: struct to free
+ * Return: 0
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d);
+}
